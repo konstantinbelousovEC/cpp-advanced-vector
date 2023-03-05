349c78bbe098026d32752981b7bbf2be888a61c2
konstantinbelousovEC <kb_sd@yahoo.com>
2023-03-05 12:40:32 +0700

    reorganization again

--- advanced-vector/vector.h
+++ advanced-vector/vector.h
@@ -1,12 +1,40 @@
 #pragma once
 
-#include "raw_memory.h"
-
 #include <cassert>
 #include <cstdlib>
 #include <new>
 #include <utility>
 #include <memory>
+
+template <typename T>
+class RawMemory {
+public:
+    RawMemory() = default;
+
+    RawMemory(size_t capacity);
+    RawMemory(const RawMemory&) = delete;
+    RawMemory& operator=(const RawMemory& rhs) = delete;
+    RawMemory(RawMemory&& other) noexcept;
+    RawMemory& operator=(RawMemory&& rhs) noexcept;
+    ~RawMemory();
+
+    T* operator+(size_t offset) noexcept;
+    const T* operator+(size_t offset) const noexcept;
+    const T& operator[](size_t index) const noexcept;
+    T& operator[](size_t index) noexcept;
+
+    void Swap(RawMemory& other) noexcept;
+    const T* GetAddress() const noexcept;
+    T* GetAddress() noexcept;
+    size_t Capacity() const noexcept;
+
+private:
+    static T* Allocate(size_t n);
+    static void Deallocate(T* buf) noexcept;
+
+    T* buffer_ = nullptr;
+    size_t capacity_ = 0;
+};
 
 template <typename T>
 class Vector {
@@ -265,4 +293,87 @@ void Vector<T>::ReplaceElementsInMemory(iterator old_memory, iterator new_memory
         std::uninitialized_copy_n(old_memory, size, new_memory);
     }
     std::destroy_n(old_memory, size);
+}
+
+// -------------------- RawMemory definitions --------------------
+
+template <typename T>
+RawMemory<T>::RawMemory(size_t capacity)
+        : buffer_(Allocate(capacity)) , capacity_(capacity) {}
+
+template <typename T>
+RawMemory<T>::RawMemory(RawMemory&& other) noexcept {
+    Swap(other);
+    Deallocate(other.buffer_);
+    other.capacity_ = 0;
+}
+
+template <typename T>
+RawMemory<T>& RawMemory<T>::operator=(RawMemory&& rhs) noexcept {
+    if (this != &rhs) {
+        Swap(rhs);
+        Deallocate(rhs.buffer_);
+        rhs.capacity_ = 0;
+    }
+    return *this;
+}
+
+template <typename T>
+RawMemory<T>::~RawMemory() {
+    if (buffer_ != nullptr) {
+        Deallocate(buffer_);
+    }
+}
+
+template <typename T>
+T* RawMemory<T>::operator+(size_t offset) noexcept {
+    assert(offset <= capacity_);
+    return buffer_ + offset;
+}
+
+template <typename T>
+const T* RawMemory<T>::operator+(size_t offset) const noexcept {
+    return const_cast<RawMemory&>(*this) + offset;
+}
+
+template <typename T>
+const T& RawMemory<T>::operator[](size_t index) const noexcept {
+    return const_cast<RawMemory&>(*this)[index];
+}
+
+template <typename T>
+T& RawMemory<T>::operator[](size_t index) noexcept {
+    assert(index < capacity_);
+    return buffer_[index];
+}
+
+template <typename T>
+void RawMemory<T>::Swap(RawMemory& other) noexcept {
+    std::swap(buffer_, other.buffer_);
+    std::swap(capacity_, other.capacity_);
+}
+
+template <typename T>
+const T* RawMemory<T>::GetAddress() const noexcept {
+    return buffer_;
+}
+
+template <typename T>
+T* RawMemory<T>::GetAddress() noexcept {
+    return buffer_;
+}
+
+template <typename T>
+size_t RawMemory<T>::Capacity() const noexcept {
+    return capacity_;
+}
+
+template <typename T>
+T* RawMemory<T>::Allocate(size_t n) {
+    return n != 0 ? static_cast<T*>(operator new(n * sizeof(T))) : nullptr;
+}
+
+template <typename T>
+void RawMemory<T>::Deallocate(T* buf) noexcept {
+    operator delete(buf);
 }
